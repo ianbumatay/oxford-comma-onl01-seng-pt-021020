4fdd3b3b6bc6fe99087b3ecfda2079299bcfb0e0
Ian  Bumatay <ianbumatay.09@gmail.com>
2020-02-22 00:41:45 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
-def oxford_comma(array)
 
+array = ["fiddleheads","okra","kohlrabi"]
+
+def oxford_comma(array)
+  array.flat_map(&:values)
+  oxford_comma = names.size > 1 ? ", " : ""
+  array.join(", ") + oxford_comma + "& #{last}"
 end
+  oxford_comma(array)
